03ef09c893501fefb9b4b3aef34cfe63d13ed20d
JudyTang <qian.tang1@thomsonreuters.com>
2014-05-14 15:32:55 +0800

    test

--- src/main/java/com/pointcarbon/esb/app/example/Main.java
+++ src/main/java/com/pointcarbon/esb/app/example/Main.java
@@ -1,16 +1,20 @@
 package com.pointcarbon.esb.app.example;
 
 public class Main {
+	
+	public static boolean flag = true;
 
     public static void main(String[] args) throws Exception {
          
-        if (args.length > 0 && "stop".equals(args[0])) {
+        if (args.length > 0 && "test".equals(args[0])) {
+        	flag = false;
             System.out.println("stopped");
             System.exit(0);
         } else {
         	
-        	while(true)
+        	while(flag)
         	{
+				Thread.sleep(5000);
         		System.out.println("Hello maven");
         		
         	}
