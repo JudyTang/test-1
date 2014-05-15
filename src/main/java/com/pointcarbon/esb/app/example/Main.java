93daa3084af306ef838f3c6fbbf7fa3f7c62d834
JudyTang <qian.tang1@thomsonreuters.com>
2014-05-15 16:18:04 +0800

    test

--- src/main/java/com/pointcarbon/esb/app/example/Main.java
+++ src/main/java/com/pointcarbon/esb/app/example/Main.java
@@ -6,21 +6,42 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
          
-        if (args.length > 0 && "test".equals(args[0])) {
-        	flag = false;
-            System.out.println("stopped");
-            System.exit(0);
-        } else {
+        if (args.length > 0 && "stop".equals(args[0])) {
         	
-        	while(flag)
-        	{
-				Thread.sleep(5000);
-        		System.out.println("Hello maven");
-        		
-        	}
+        	flag = false;
+
+        } else {
             
-            
+            new Thread(new Test()).start();
         }
     }
+    
 
+    
+
+}
+
+class Test implements Runnable {
+
+	
+	public void run()
+	{
+		if(!Main.flag)
+		{
+			System.out.println("I'm stopping");
+			return;
+			
+		}	
+		
+		try {
+			System.out.println("I'm running");
+			Thread.sleep(5000);
+			new Thread(new Test()).start();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
 }
